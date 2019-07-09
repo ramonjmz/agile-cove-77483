953cc275d99595e7ec99506e4b9c907fc591d8ef
ramomjmz <ramonjmx@gmail.com>
2019-07-08 22:43:34 -0500

    token-devices

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/token-devices', function(req, res){
-	res.send(req.body.token);
-});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/token', function(req, res){
-	res.send(req.body.token); })
+  .get('/token', res.send(req.body.token))
   .get('/switch', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
