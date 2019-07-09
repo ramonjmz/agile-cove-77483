84909579e71110492b751e6690a7a7b3b6ce0a93
ramomjmz <ramonjmx@gmail.com>
2019-07-08 21:42:25 -0500

    error

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
 var bodyParser = require("body-parser");
-	.use(bodyParser.json());
-	.use(bodyParser.urlencoded({ extended: true }));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: true }));
 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/switch', (req, res) => res.render('pages/index'))
-
-
-   .post('/token-device', function(req, res){
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/switch', (req, res) => res.render('pages/index'))
+  app.post('/token-device', function(req, res){
   	res.send(req.body.token);
   });
 
