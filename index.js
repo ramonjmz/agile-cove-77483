037c8f5b1fb2f8cce954ad144103eeaf3710b54b
ramomjmz <ramonjmx@gmail.com>
2019-07-08 21:51:10 -0500

    error

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
+const app = express()
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-var bodyParse = require("body-parser");
-app.use(bodyParse.json());
-app.use(bodyParse.urlencoded({extended: true }));
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true }));
 
 
 express()
