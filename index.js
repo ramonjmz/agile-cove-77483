b56cbe12594e8869176d40c0b115e34fed89fde9
ramomjmz <ramonjmx@gmail.com>
2019-07-08 23:19:40 -0500

    agregando cambios

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const bodyParser = require("body-parser");
+
 express()
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/token-devices', (req, res) => res.send(request.body.token))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
