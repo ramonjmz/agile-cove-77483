d125e2a95b204fb50b9ccb3505483b3f4255e7bc
ramomjmz <ramonjmx@gmail.com>
2019-07-08 21:49:44 -0500

    error

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+var bodyParse = require("body-parser");
+app.use(bodyParse.json());
+app.use(bodyParse.urlencoded({extended: true }));
+
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/switch', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
