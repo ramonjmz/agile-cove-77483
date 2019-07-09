115b4e350c88f92e2adb4cc88d9646fbb16b58d0
ramomjmz <ramonjmx@gmail.com>
2019-07-08 21:43:27 -0500

    error

--- index.js
+++ index.js
@@ -7,15 +7,14 @@ var bodyParser = require("body-parser");
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
 
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   app.get('/', (req, res) => res.render('pages/index'))
   app.get('/switch', (req, res) => res.render('pages/index'))
   app.post('/token-device', function(req, res){
   	res.send(req.body.token);
   });
 
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
