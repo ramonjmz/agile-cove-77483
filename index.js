76859e4abff3dd845db4a4187a8b32537820b8b9
ramomjmz <ramonjmx@gmail.com>
2019-07-09 00:19:34 -0500

    agregando cambios2

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ express()
   .get('/cool', (req, res) => res.send(cool()))
   .get('/token-devices', (req, res) => res.send(req.body.token))
   .get('/switch', (req, res) => res.json({
-            data: req.body.ping}))
+            data: req.params.ping}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
