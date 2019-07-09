ff1f5aae2014330e86150a13550a191c19af6654
ramomjmz <ramonjmx@gmail.com>
2019-07-08 22:02:17 -0500

    token-devices

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true }));
 
-app.post('/token-devices', (req, res){
+app.post('/token-devices', function(req, res){
 	res.send(req.body.token);
 }):
 
