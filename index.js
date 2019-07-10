7a14e5f61679994a186bcf0ebcad5992936b4c66
ramomjmz <ramonjmx@gmail.com>
2019-07-09 23:56:12 -0500

    q2

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ express()
       const client = await pool.connect()
       const result = await client.query('SELECT status FROM device where name = $1', [req.params.device]);
       //const results = { 'results': (result) ? result.rows[0].status : null};
-      const estado (result.rows[0].status) ? 1 : 0;
+      const estado = (result.rows[0].status) ? 1 : 0;
       // res.render('pages/db', results );
       console.log(results);
       res.format ({
