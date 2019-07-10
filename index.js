912ec3da210065ead6ff971c5db30631680f67cd
ramomjmz <ramonjmx@gmail.com>
2019-07-09 19:10:52 -0500

    agregando cambios

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 5000
 
 const bodyParser = require("body-parser");
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
@@ -13,8 +19,23 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .get('/token-devices', (req, res) => res.send(req.params.token))
   .get('/switch', (req, res) => res.json({
             data: req.params.foco}))
   .get('/foco', (req, res) => res.send(1))
+
+  .put('/foco', (req, res) => res.send(1))
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
